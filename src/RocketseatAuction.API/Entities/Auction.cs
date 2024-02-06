d38cc503a063895ae1c4bf7328a7f7dfd43f45c9
Bruno Gaspar <bruninhoogaspar@gmail.com>
2024-02-06 17:30:54 -0300

    feat: create api, add auction, add offer

---
+++ src/RocketseatAuction.API/Entities/Auction.cs
@@ -0,0 +1,11 @@
+﻿namespace RocketseatAuction.API.Entities;
+
+public class Auction
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Starts { get; set; } = DateTime.Now;
+    public DateTime Ends { get; set; }
+
+    public List<Item> Items { get; set; } = [];
+}
